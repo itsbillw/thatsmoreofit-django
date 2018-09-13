d303726b4cda277fae58677aa1c85b2e283dac08
itsbillw <itsbillw@gmail.com>
2018-09-13 20:54:09 +0000

    carousel gallery

--- gallery/views.py
+++ gallery/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 def index(request):
     """The gallery page for thatsmoreofit"""
     return render(request, 'gallery/index.html')
-
-def album(request):
-    return render(request, 'gallery/album.html')
