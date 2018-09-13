dcbc341d9df3af0f445a49a9c1752a9f5c595d6d
itsbillw <itsbillw@gmail.com>
2018-09-13 00:06:25 +0000

    first commit

---
+++ gallery/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'gallery'
+urlpatterns = [
+    # gallery page.
+    path('', views.index, name='index'),
+]
---
+++ gallery/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def index(request):
+    """The gallery page for thatsmoreofit"""
+    return render(request, 'gallery/index.html')
