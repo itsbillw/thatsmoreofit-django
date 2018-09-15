0b551b3bdc87bd30a967aeff86fe368ae629a81d
itsbillw <itsbillw@gmail.com>
2018-09-15 19:10:33 +0000

    adding tracker to data app update3

--- thatsmoreofit/settings.py
+++ thatsmoreofit/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/home/itsbillw/thatsmoreofit/static'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = '/home/itsbillw/thatsmoreofit/static'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Settings for django-bootstrap4
 BOOTSTRAP4 = {
