49978986ad183f4e4e86263145d0a1a48c971ce6
itsbillw <itsbillw@gmail.com>
2018-11-16 03:02:44 +0000

    test

--- thatsmoreofit/settings.py
+++ thatsmoreofit/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '578x4)_u-avyb9q%gjc@a1@j0rjduf^7g-=zp$^at#=97elay7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['itsbillw.pythonanywhere.com','*']
+ALLOWED_HOSTS = ['www.thatsmoreofit.com','itsbillw.pythonanywhere.com','*']
 
 
 # Application definition
